17f6c56b1efee84c5eb35263797d2630f4bf1a97
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-28 09:30:03 +0600

    don't getting it

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,36 @@
-const loadData = async () =>{
+const loadData = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/ai/tools')
     const data = await res.json();
-    const datas = data.data.tools[0];
-    console.log(desc)
+    const techs = data.data;
+    // console.log(data.data.tools)
+    displayData(techs)
+}
+
+
+const displayData = techs => {
+    const mainContainer = document.getElementById('container');
+    techs.forEach(tech => {
+        const eachCardContainter = document.createElement('div');
+        eachCardContainter.classList = `card shadow-xl`;
+        eachCardContainter.innerHTML = `
+        <figure><img src="${tech.image}" alt="Shoes" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">Features</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <hr>
+          <div class="flex justify-between items-center">
+            <div class="">
+                <h1>ChatGPT</h1>
+                <p>relseat date</p>
+            </div>
+            <div class="">
+                <button class="btn">&rarr;</button>
+            </div>
+        `;
+        console.log(tech.image)
+        mainContainer.appendChild(eachCardContainter);
+
+    });
 }
 
 loadData()
